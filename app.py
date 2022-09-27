b00f0102fd6aa16a032180a955c0f75ff9dd3d6c
Patrick Arruda <patrick@plugify.com.br>
2022-09-26 22:05:57 -0300

    feat: oraganization by folders

--- app.py
+++ app.py
@@ -2,28 +2,19 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 
-# from models import User as ModelUser
-# from schema import User as SchemaUser
+from routes.users import router as users_router
 
-from models.users import User as ModelUser
-from schemas.users import User as SchemaUser
 
 import os
 from dotenv import load_dotenv
 
-load_dotenv('.env')
+load_dotenv()
 
 
-app = FastAPI()
+app = FastAPI(title='Challenge EY - API',
+              description='API para o desafio da EY',
+              version='1.0.0')
 
-# to avoid csrftokenError
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
-@app.get('/users/',response_model=list[SchemaUser])
-async def users():
-    users = db.session.query(ModelUser).all()
-    return users
-
-# To run locally
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+app.include_router(users_router, prefix='/users', tags=['users'])
