ce29da7d735d51948312acf578ec4a61ddf9ccca
Patrick Arruda <patrick@plugify.com.br>
2022-09-28 01:11:30 -0300

    CORS

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.users import router as users_router
 
@@ -13,6 +14,21 @@ load_dotenv()
 app = FastAPI(title='Challenge EY - API',
               description='API para o desafio da EY',
               version='1.0.0')
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "challenge-ey.web.app",
+    "challenge-ey.firebaseapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
